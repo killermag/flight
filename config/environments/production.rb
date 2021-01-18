18d9ac3d3b944b65370e2423abb19e76a2b69a02
killermag <galahaal580@gmail.com>
2021-01-18 00:28:25 -0500

    typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,7 +119,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.perform_deliveries = true 
-  config.action_mailer.url_options = { host: "odinairs.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "odinairs.herokuapp.com" }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
